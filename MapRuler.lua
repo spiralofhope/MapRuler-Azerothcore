a14f1e74949b076af8a062db0bf39596d61221a4
Your Name <you@example.com>
2024-12-16 23:58:36 -0800

    Style cleanup

--- MapRuler.lua
+++ MapRuler.lua
@@ -1,5 +1,5 @@
-local dots = {} --stores all the dot textures.  Each dot is a separate texture
-local dotSpacing = 9 --the blank space between dots
+local dots = {} -- Stores all the dot textures.  Each dot is a separate texture
+local dotSpacing = 9 -- The blank space between dots
 local playerX = 0
 local playerY = 0
 local playerFacing = 0
@@ -8,99 +8,80 @@ local timeSinceLastUpdate = 0
 local maxLineLength
 
 
-function MapRulerFrame_Init(self)
-	--setup the frame to overlay the map detail frame
-	self:SetWidth(self:GetParent():GetWidth())
-	self:SetHeight(self:GetParent():GetHeight())
+function MapRulerFrame_Init( self )
+	-- Setup the frame to overlay the map detail frame
+	self:SetWidth(  self:GetParent():GetWidth() )
+	self:SetHeight( self:GetParent():GetHeight() )
 	
-	--max line length we'll ever need is from one corner to the other of the currently displayed map.
-	--simple distance formula
-	maxLineLength = math.sqrt(((WorldMapDetailFrame:GetHeight()) ^ 2) + ((WorldMapDetailFrame:GetWidth()) ^ 2))
+	-- The max line length we'll ever need is from one corner to the other of the currently displayed map.
+	-- Simple distance formula
+	maxLineLength = math.sqrt( ( ( WorldMapDetailFrame:GetHeight() ) ^ 2 ) + ( ( WorldMapDetailFrame:GetWidth()) ^ 2 ) )
 	
-	--setup the textures once and only once
+	-- Setup the textures once and only once
 	for i = 0, maxLineLength, dotSpacing do
 		dots[i] = self:CreateTexture()
+	end
+end
 
-	end --for
+
+-- Fires on frame update
+function MapRuler_OnUpdate( self, elapsed )
+
+	-- Inrcement the time
+	timeSinceLastUpdate = timeSinceLastUpdate + elapsed
+
+	local percentX, percentY = GetPlayerMapPosition( 'player' )  -- Returns the player position as a percentage from top left
 	
-end --MapRulerFrame_Init()
-
-
---fires on frame update
-function MapRuler_OnUpdate(self, elapsed) 
-
-	--incement the time
-	timeSinceLastUpdate = timeSinceLastUpdate + elapsed;
-
-	local percentX, percentY = GetPlayerMapPosition("player") --returns the player position as a percentage from top left
-	
-	--only fire on the interval and if the player is on the current map
-	if (timeSinceLastUpdate > updateInterval) and (percentX ~= 0 and percentY ~= 0) and GetPlayerFacing() then
+	-- Only fire on the interval and if the player is on the current map
+	if ( timeSinceLastUpdate > updateInterval ) and ( percentX ~= 0 and percentY ~= 0 ) and GetPlayerFacing() then
 		
 		-- 0 is due north on the map 
-		-- have to add pi/2 to get back to where the player is pointed (angle in radians)
-		local angle = GetPlayerFacing() + (math.pi)/2  
-
-		--check to see if the player has moved or changed facing
-		if (percentX ~= playerX or percentY ~= playerY or playerFacing ~= angle) then
-			--save the current position and facing so we can compare it next time
+		-- Have to add pi/2 to get back to where the player is pointed (angle in radians)
+		local angle = GetPlayerFacing() + ( math.pi ) / 2
+    
+		-- Check to see if the player has moved or changed facing
+		if ( percentX ~= playerX
+    or   percentY ~= playerY
+    or   playerFacing ~= angle 
+    ) then
+			-- Save the current position and facing so we can compare it next time
 			playerX = percentX
 			playerY = percentY
 			playerFacing = angle
 
-			-- (percentX * self:GetWidth()) ,  (percentY * self:GetHeight()) is the pixel location of the player on the map
+			-- ( percentX * self:GetWidth() ) ,  ( percentY * self:GetHeight() ) is the pixel location of the player on the map
 			-- y-coords are kind of reversed in that the top of the screen is 0 so multiply it by -1
 			showLine(self, percentX * self:GetWidth(), percentY * self:GetHeight() * -1, angle)
 		end --inner if
-		timeSinceLastUpdate = 0 --the interval was achived so reset it
-	else --top if
-		--Make sure the player is not on the current map or in an instance
-		if ((percentX == 0 and percentY == 0) or not GetPlayerFacing()) then
+		timeSinceLastUpdate = 0  -- The interval was achived so reset it
+	else
+		-- Make sure the player is not on the current map or in an instance
+		if ( ( percentX == 0 and percentY == 0 ) or not GetPlayerFacing() ) then
 			hideLine()
 		end
-	end --top if-else
-	
-end --MapRuler_OnUpdate()
+	end
+end  -- MapRuler_OnUpdate()
 
 
---Places the dots on the line
-function showLine(parentFrame, startX, startY, angle)
-	--set the values here so I'm not calculating them for each dot
-	xComponent = math.cos(angle) -- x-component of the vector
-	yComponent = math.sin(angle) -- ycomponent of the vector
-	
+-- Places the dots on the line:
+function showLine( parentFrame, startX, startY, angle )
+	-- Set the values here so I'm not calculating them for each dot:
+	xComponent = math.cos( angle )
+	yComponent = math.sin( angle )
+
 	for i = 0, maxLineLength, dotSpacing do
-		dots[i]:SetAlpha(MapRulerOptions["opacity"])
-		dots[i]:SetColorTexture(MapRulerOptions["red"],MapRulerOptions["green"],MapRulerOptions["blue"])
-		dots[i]:SetHeight(MapRulerOptions["size"])
-		dots[i]:SetWidth(MapRulerOptions["size"])
-		dots[i]:SetPoint("TOPLEFT", startX + (xComponent * i), startY + (yComponent * i))
+		dots[i]:SetAlpha( MapRulerOptions[ 'opacity' ] )
+    dots[i]:SetColorTexture(MapRulerOptions["red"],MapRulerOptions["green"],MapRulerOptions["blue"])
 		
-	end --for
-
-end --line()
-
-
---hide the line (just like the name says)
-function hideLine()
-	for i = 0, maxLineLength, dotSpacing do
-		dots[i]:SetAlpha(0)
-	end --for
+		dots[i]:SetHeight( MapRulerOptions[ 'size' ] )
+		dots[i]:SetWidth(  MapRulerOptions[ 'size' ] )
+		dots[i]:SetPoint( 'TOPLEFT', startX + ( xComponent * i ), startY + ( yComponent * i ) )
+	end
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function hideLine()
+	for i = 0, maxLineLength, dotSpacing do
+		dots[ i ]:SetAlpha( 0 )
+	end
+end
--- MapRuler_Options.lua
+++ MapRuler_Options.lua
@@ -6,296 +6,247 @@ dot spacing
 dot size
 --]]
 
---a table to hold all the wonderful options
---must be declared before the client tries to use it 
 MapRulerOptions = {}
 
---setup Options Frame
-local MROptionsFrame = CreateFrame("Frame", "Map Ruler", InterfaceOptionsFramePanelContainer);
-MROptionsFrame.name = "Map Ruler"
+-- Setup Options Frame
+local MROptionsFrame = CreateFrame( 'Frame', 'Map Ruler', InterfaceOptionsFramePanelContainer )
+MROptionsFrame.name = 'Map Ruler'
 MROptionsFrame.default = MRSetDefaultOptions
-MROptionsFrame:SetPoint("CENTER")
-MROptionsFrame:SetSize(InterfaceOptionsFrame:GetWidth(), InterfaceOptionsFrame:GetHeight())
+MROptionsFrame:SetPoint( 'CENTER' )
+MROptionsFrame:SetSize( InterfaceOptionsFrame:GetWidth(), InterfaceOptionsFrame:GetHeight() )
 
---load the frame but don't display it, also runs the OnShow handler
+-- Load the frame but don't display it, also runs the OnShow handler
 MROptionsFrame:Hide()
 
-MROptionsFrame:RegisterEvent("ADDON_LOADED")
+MROptionsFrame:RegisterEvent( 'ADDON_LOADED' )
 
-local OpacityEditBox = CreateFrame("EditBox", nil, MROptionsFrame)
-local MROpacitySlider = CreateFrame("Slider", "MROSlider", MROptionsFrame, "OptionsSliderTemplate")
-local ColorButton = CreateFrame("CheckButton", "MRColorButton", MROptionsFrame, "UIPanelButtonTemplate")
-local ResetButton = CreateFrame("CheckButton", "MRResetButton", MROptionsFrame, "UIPanelButtonTemplate")
-local SizeSlider = CreateFrame("Slider", "MRSizeSlider", MROptionsFrame, "OptionsSliderTemplate")
-local SizeBox = CreateFrame("EditBox", nil, MROptionsFrame)
+local OpacityEditBox  = CreateFrame( 'EditBox',      nil,            MROptionsFrame )
+local MROpacitySlider = CreateFrame( 'Slider',      'MROSlider',     MROptionsFrame, 'OptionsSliderTemplate' )
+local ColorButton     = CreateFrame( 'CheckButton', 'MRColorButton', MROptionsFrame, 'UIPanelButtonTemplate' )
+local ResetButton     = CreateFrame( 'CheckButton', 'MRResetButton', MROptionsFrame, 'UIPanelButtonTemplate' )
+local SizeSlider      = CreateFrame( 'Slider',      'MRSizeSlider',  MROptionsFrame, 'OptionsSliderTemplate' )
+local SizeBox         = CreateFrame( 'EditBox',      nil,            MROptionsFrame )
 
 
-local function MROpacitySliderOnValueChanged(self, value)
-	if(MapRulerOptions["opacity"] ~= nil and value ~= MapRulerOptions["opacity"]) then
-		MapRulerOptions["opacity"] = math.floor(value) / 100
-		OpacityEditBox:SetText(MapRulerOptions["opacity"] * 100)
-	end --if
-end --MROptionsOpacityHandler()
+local function MROpacitySliderOnValueChanged( self, value )
+	if( MapRulerOptions[ 'opacity' ] ~= nil
+    and value ~= MapRulerOptions[ 'opacity' ]
+  ) then
+		MapRulerOptions[ 'opacity' ] = math.floor( value ) / 100
+		OpacityEditBox:SetText( MapRulerOptions[ 'opacity' ] * 100 )
+	end
+end
 
 
 local function OpacityEditBoxEnterPressed(self)
-	--only allow numbers from 0 to 100
-	local theNumber = tonumber(self:GetText())
-	if(theNumber > 100) then
-		theNumber = 100
-	end
-	
-	if(theNumber < 0) then
-		theNumber = 0
-	end
-	
-	self:SetText(theNumber)
-	
-	--when the slider value is changed MapRulerOptions["opacity"] will be updated
-	MROpacitySlider:SetValue(theNumber)
+	-- Only allow numbers from 0 to 100:
+	local n = tonumber(self:GetText())
+	if( n > 100 )  then  n = 100  end
+	if( n <   0 )  then  n =   0  end
+	self:SetText( n )
+  
+	-- When the slider value is changed MapRulerOptions[ 'opacity' ] will be updated.
+	MROpacitySlider:SetValue( n )
 	self:ClearFocus()
-end --OpacityEditBoxEnterPressed()
+end
 
 
 local function setupMROpacitySlider()
 	MROpacitySlider:ClearAllPoints()
-	MROpacitySlider:SetPoint("TOPLEFT", MROptionsFrame, "TOPLEFT", 50, -80)
-	MROpacitySlider:SetValueStep(1)
-	MROpacitySlider:SetMinMaxValues(0, 100)
-	MROpacitySlider:SetValue(MapRulerOptions["opacity"] * 100)
-	--MROpacitySlider.tooltipText = "You are here" --Creates a tooltip on mouseover.
-	MROpacitySlider:SetScript("OnValueChanged", MROpacitySliderOnValueChanged)
+	MROpacitySlider:SetPoint( 'TOPLEFT', MROptionsFrame, 'TOPLEFT', 50, -80 )
+	MROpacitySlider:SetValueStep( 1 )
+	MROpacitySlider:SetMinMaxValues( 0, 100 )
+	MROpacitySlider:SetValue(MapRulerOptions[ 'opacity' ] * 100 )
+	-- MROpacitySlider.tooltipText = "You are here"  -- Creates a tooltip on mouseover.
+	MROpacitySlider:SetScript( 'OnValueChanged', MROpacitySliderOnValueChanged )
 	
-	getglobal(MROpacitySlider:GetName() .. 'Low'):SetText('0') --Sets the left-side slider text
-	getglobal(MROpacitySlider:GetName() .. 'High'):SetText('100') --Sets the right-side slider text
-	getglobal(MROpacitySlider:GetName() .. 'Text'):SetText('Opacity') --Sets the "title" text
-end --setupMROpacitySlider()
+	getglobal( MROpacitySlider:GetName() .. 'Low'  ):SetText(       '0' )  -- Sets the left-side slider text
+	getglobal( MROpacitySlider:GetName() .. 'High' ):SetText(     '100' )  -- Sets the right-side slider text
+	getglobal( MROpacitySlider:GetName() .. 'Text' ):SetText( 'Opacity' )  -- Sets the "title" text
+end
 
 
 local function setupOpacityBox()
-	
 	OpacityEditBox:ClearAllPoints()
-	OpacityEditBox:SetWidth(30)
-	OpacityEditBox:SetHeight(20)
-	OpacityEditBox:SetFontObject("GameFontHighlight")
-	OpacityEditBox:SetMaxLetters(3)
-	OpacityEditBox:SetJustifyH("CENTER")
-	OpacityEditBox:SetScript("OnEnterPressed", OpacityEditBoxEnterPressed)
-	OpacityEditBox:SetNumeric(true) 
+	OpacityEditBox:SetWidth( 30 )
+	OpacityEditBox:SetHeight( 20 )
+	OpacityEditBox:SetFontObject( 'GameFontHighlight' )
+	OpacityEditBox:SetMaxLetters( 3 )
+	OpacityEditBox:SetJustifyH( 'CENTER' )
+	OpacityEditBox:SetScript( 'OnEnterPressed', OpacityEditBoxEnterPressed )
+	OpacityEditBox:SetNumeric( true )
 	
-	--does having this in here create a reference to backdrop that can never be cleared?
+	-- Does having this in here create a reference to backdrop that can never be cleared?
 	local backdrop = {
-				bgFile = nil, 
-				edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
+				bgFile = nil,
+				edgeFile = 'Interface/Tooltips/UI-Tooltip-Border',
 				tile = false,
 				tileSize = 0,
 				edgeSize = 3,
 				insets = { left = 0, right = 0, top = 0, bottom = 0 }
 	}
 	
-	OpacityEditBox:SetBackdrop(backdrop)
-	OpacityEditBox:SetPoint("TOPLEFT", MROptionsFrame, "TOPLEFT", 220, -80)
-	OpacityEditBox:SetAutoFocus(false)
+	OpacityEditBox:SetBackdrop( backdrop )
+	OpacityEditBox:SetPoint( 'TOPLEFT', MROptionsFrame, 'TOPLEFT', 220, -80 )
+	OpacityEditBox:SetAutoFocus( false )
 	OpacityEditBox:ClearFocus()
-	OpacityEditBox:SetText(MapRulerOptions["opacity"] * 100)
-end --setupOpacityBox()
+	OpacityEditBox:SetText( MapRulerOptions[ 'opacity' ] * 100 )
+end
 
 
-local function MRColorCallback(restore)
-	local newR, newG, newB, newA;
+local function MRColorCallback( restore )
+	local newR, newG, newB, newA
 	if restore then
 		-- The user bailed, we extract the old color from the table created by ShowColorPicker.
-		newR, newG, newB, newA = unpack(restore);
+		newR, newG, newB, newA = unpack( restore )
 	else
-		newA, newR, newG, newB = OpacitySliderFrame:GetValue(), ColorPickerFrame:GetColorRGB();
+		newA, newR, newG, newB = OpacitySliderFrame:GetValue(), ColorPickerFrame:GetColorRGB()
 	end
 
-	MapRulerOptions["red"],MapRulerOptions["green"],MapRulerOptions["blue"],MapRulerOptions["opacity"] = 
+	MapRulerOptions[ 'red' ],MapRulerOptions[ 'green' ],MapRulerOptions[ 'blue' ],MapRulerOptions[ 'opacity' ] = 
 		newR, newG, newB, newA
 	
-	--this will also change the opacity textbox
-	MROpacitySlider:SetValue(MapRulerOptions["opacity"] * 100)
-	
+	-- This will also change the opacity textbox:
+	MROpacitySlider:SetValue( MapRulerOptions[ 'opacity' ] * 100 )
 end
 
 
 local function ColorButtonClicked()
-
-	ColorPickerFrame:SetColorRGB(MapRulerOptions["red"], MapRulerOptions["green"], MapRulerOptions["blue"])
-	
-	ColorPickerFrame.hasOpacity, ColorPickerFrame.opacity = 
-		(MapRulerOptions["opacity"] ~= nil), MapRulerOptions["opacity"]
-	
-	ColorPickerFrame.previousValues = 
-		{MapRulerOptions["red"],MapRulerOptions["green"],
-		MapRulerOptions["blue"],MapRulerOptions["opacity"]}
-	
-	ColorPickerFrame.func, ColorPickerFrame.opacityFunc, ColorPickerFrame.cancelFunc = 
-		MRColorCallback, MRColorCallback, MRColorCallback
-	
-	ColorPickerFrame:Hide() -- Need to run the OnShow handler.
+	ColorPickerFrame:SetColorRGB(MapRulerOptions['red'], MapRulerOptions['green'], MapRulerOptions['blue'])
+	ColorPickerFrame.hasOpacity, ColorPickerFrame.opacity = ( MapRulerOptions['opacity'] ~= nil ), MapRulerOptions[ 'opacity' ]
+	ColorPickerFrame.previousValues = {
+    MapRulerOptions[ 'red'  ], MapRulerOptions[ 'green' ],
+		MapRulerOptions[ 'blue' ], MapRulerOptions[ 'opacity' ]
+  }
+	ColorPickerFrame.func        = MRColorCallback
+	ColorPickerFrame.opacityFunc = MRColorCallback
+	ColorPickerFrame.cancelFunc  = MRColorCallback
+	ColorPickerFrame:Hide()  -- Need to run the OnShow handler.
 	ColorPickerFrame:Show()
-	
 end
 
 
 local function setupColorButton()
 	ColorButton:ClearAllPoints()
-	ColorButton:SetPoint("TOPLEFT", MROptionsFrame, "TOPLEFT", 50, -140)
-	ColorButton:SetHeight(30)
-	ColorButton:SetWidth(150)
-	ColorButton:SetText("Change Color")
-	ColorButton:SetScript("OnClick", ColorButtonClicked)
-end --setupColorButton()
+	ColorButton:SetPoint( 'TOPLEFT', MROptionsFrame, 'TOPLEFT', 50, -140 )
+	ColorButton:SetHeight( 30 )
+	ColorButton:SetWidth( 150 )
+	ColorButton:SetText( 'Change Color' )
+	ColorButton:SetScript( 'OnClick', ColorButtonClicked )
+end
 
 
 local function ResetButtonClicked()
-
-	MapRulerOptions["opacity"] = .5
-	MapRulerOptions["red"] = 0
-	MapRulerOptions["green"] = 0
-	MapRulerOptions["blue"] = 0
-	MapRulerOptions["size"] = 3
-	MapRulerOptions["spacing"] = 9
-	--this will also change the opacity textbox
-	MROpacitySlider:SetValue(MapRulerOptions["opacity"] * 100)
-	SizeSlider:SetValue(MapRulerOptions["size"])
-	
-end --end ResetButtonClicked()
+	MapRulerOptions[ 'opacity' ] = .5
+	MapRulerOptions[ 'red' ] = 0
+	MapRulerOptions[ 'green'] = 0
+	MapRulerOptions[ 'blue' ] = 0
+	MapRulerOptions[ 'size' ] = 3
+	MapRulerOptions[ 'spacing' ] = 9
+	-- This will also change the opacity textbox:
+	MROpacitySlider:SetValue( MapRulerOptions[ 'opacity' ] * 100 )
+	SizeSlider:SetValue( MapRulerOptions[ 'size' ] )
+end
 
 
 local function setupResetButton()
 	ResetButton:ClearAllPoints()
-	ResetButton:SetPoint("TOPLEFT", MROptionsFrame, "TOPLEFT", 215, -140)
-	ResetButton:SetHeight(30)
-	ResetButton:SetWidth(150)
-	ResetButton:SetText("Restore Defaults")
-	ResetButton:SetScript("OnClick", ResetButtonClicked)
+	ResetButton:SetPoint( 'TOPLEFT', MROptionsFrame, 'TOPLEFT', 215, -140 )
+	ResetButton:SetHeight( 30 )
+	ResetButton:SetWidth( 150 )
+	ResetButton:SetText( 'Restore Defaults' )
+	ResetButton:SetScript( 'OnClick', ResetButtonClicked )
 end
 
 
-local function MRSizeSliderOnValueChanged(self, value)
-	if(MapRulerOptions["size"] ~= nil and value ~= MapRulerOptions["size"]) then
-		MapRulerOptions["size"] = math.floor(value)
-		SizeBox:SetText(MapRulerOptions["size"])
-	end --if
-end --end MRSizeSliderOnValueChanged()
+local function MRSizeSliderOnValueChanged( self, value )
+	if( MapRulerOptions['size'] ~= nil
+  and value                   ~= MapRulerOptions['size']
+  ) then
+		MapRulerOptions['size'] = math.floor( value )
+		SizeBox:SetText( MapRulerOptions[ 'size' ] )
+	end
+end
 
 
 local function setupSizeSlider()
 	SizeSlider:ClearAllPoints()
-	SizeSlider:SetPoint("TOPLEFT", MROptionsFrame, "TOPLEFT", 50, -30)
-	SizeSlider:SetValueStep(1)
-	SizeSlider:SetMinMaxValues(1, 10)
-	SizeSlider:SetValue(MapRulerOptions["size"])
-	SizeSlider:SetScript("OnValueChanged", MRSizeSliderOnValueChanged)
-	
-	getglobal(SizeSlider:GetName() .. 'Low'):SetText('1') --Sets the left-side slider text
-	getglobal(SizeSlider:GetName() .. 'High'):SetText('10') --Sets the right-side slider text
-	getglobal(SizeSlider:GetName() .. 'Text'):SetText('Dot Size') --Sets the "title" text
-end --end setupSizeSlider()
+	SizeSlider:SetPoint( 'TOPLEFT', MROptionsFrame, 'TOPLEFT', 50, -30 )
+	SizeSlider:SetValueStep( 1 )
+	SizeSlider:SetMinMaxValues( 1, 10 )
+	SizeSlider:SetValue( MapRulerOptions[ 'size' ] )
+	SizeSlider:SetScript( 'OnValueChanged', MRSizeSliderOnValueChanged )
+	getglobal( SizeSlider:GetName() .. 'Low'  ):SetText( '1' )        -- Sets the left-side slider text
+	getglobal( SizeSlider:GetName() .. 'High' ):SetText( '10' )       -- Sets the right-side slider text
+	getglobal( SizeSlider:GetName() .. 'Text' ):SetText( 'Dot Size' ) -- Sets the 'title' text
+end
 
 
 local function SizeBoxEnterPressed()
-
-	--only allow numbers from 0 to 10
-	local theNumber = tonumber(SizeBox:GetText())
-	if(theNumber > 10) then
-		theNumber = 10
-	end
-	
-	if(theNumber < 0) then
-		theNumber = 0
-	end
-	
-	SizeBox:SetText(theNumber)
-	
-	--when the slider value is changed MapRulerOptions["size"] will be updated
-	SizeSlider:SetValue(theNumber)
-
+	-- Only allow numbers from 0 to 10:
+	local n = tonumber(SizeBox:GetText())
+	if( n > 10 ) then  n = 10  end
+	if( n <  0 ) then  n =  0  end
+	SizeBox:SetText( n )
+  
+	-- When the slider value is changed MapRulerOptions[ 'size' ] will be updated
+	SizeSlider:SetValue( n )
 	SizeBox:ClearFocus()
-
-end --SizeBoxEnterPressed()
+end
 
 
 local function setupSizeBox()
 	SizeBox:ClearAllPoints()
 	
-	SizeBox:SetWidth(30)
-	SizeBox:SetHeight(20)
-	SizeBox:SetFontObject("GameFontHighlight")
-	SizeBox:SetMaxLetters(3)
-	SizeBox:SetJustifyH("CENTER")
-	SizeBox:SetScript("OnEnterPressed", SizeBoxEnterPressed)
-	SizeBox:SetNumeric(true) 
+	SizeBox:SetWidth( 30 )
+	SizeBox:SetHeight( 20 )
+	SizeBox:SetFontObject( 'GameFontHighlight' )
+	SizeBox:SetMaxLetters( 3 )
+	SizeBox:SetJustifyH( 'CENTER' )
+	SizeBox:SetScript( 'OnEnterPressed', SizeBoxEnterPressed )
+	SizeBox:SetNumeric( true )
 	
-	--does having this in here create a reference to backdrop that can never be cleared?
+	-- Does having this in here create a reference to backdrop that can never be cleared?
 	local backdrop = {
-				bgFile = nil, 
-				edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
-				tile = false,
-				tileSize = 0,
-				edgeSize = 3,
-				insets = { left = 0, right = 0, top = 0, bottom = 0 }
+    bgFile = nil, 
+    edgeFile = 'Interface/Tooltips/UI-Tooltip-Border', 
+    tile = false,
+    tileSize = 0,
+    edgeSize = 3,
+    insets = { left = 0, right = 0, top = 0, bottom = 0 }
 	}
 	
-	SizeBox:SetBackdrop(backdrop)
-	SizeBox:SetPoint("TOPLEFT", MROptionsFrame, "TOPLEFT", 220, -30)
-	SizeBox:SetAutoFocus(false)
+	SizeBox:SetBackdrop( backdrop )
+	SizeBox:SetPoint( 'TOPLEFT', MROptionsFrame, 'TOPLEFT', 220, -30 )
+	SizeBox:SetAutoFocus( false )
 	SizeBox:ClearFocus()
-	SizeBox:SetText(MapRulerOptions["size"])
-end --setupSizeBox()
+	SizeBox:SetText( MapRulerOptions[ 'size' ] )
+end
 
 
-local function MROptionsEventHandler (self, event, ...)
-	if event == "ADDON_LOADED" and ... == "MapRuler" then
-		--see if it acutally loaded anything
-		if MapRulerOptions["spacing"] == nil then --set these defaults
-			MapRulerOptions["opacity"] = .5
-			MapRulerOptions["red"] = 0
-			MapRulerOptions["green"] = 0
-			MapRulerOptions["blue"] = 0
-			MapRulerOptions["size"] = 3
-			MapRulerOptions["spacing"] = 9
-		end --if
+local function MROptionsEventHandler( self, event, ... )
+	if event == 'ADDON_LOADED' and ... == 'MapRuler' then
+		-- If nothing is loaded, then set defaults
+		if MapRulerOptions[ 'spacing'] == nil then
+			MapRulerOptions[ 'spacing' ] = 9
+			MapRulerOptions[ 'opacity' ] = .5
+			MapRulerOptions[ 'red' ] = 0
+			MapRulerOptions[ 'green' ] = 0
+			MapRulerOptions[ 'blue' ] = 0
+			MapRulerOptions[ 'size' ] = 3
+		end
 		setupMROpacitySlider()
 		setupOpacityBox()
 		setupColorButton()
 		setupResetButton()
 		setupSizeSlider()
 		setupSizeBox()
-	end --if event
-	
-end --eventHandler()
+	end
+end
 
 
---this must be after the declaration of MROptionsEventHandler
-MROptionsFrame:SetScript("OnEvent", MROptionsEventHandler)
+-- This must be after the declaration of MROptionsEventHandler
+MROptionsFrame:SetScript( 'OnEvent', MROptionsEventHandler )
 
---register the addon in the blizz interface window
-InterfaceOptions_AddCategory(MROptionsFrame)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Register the addon in the blizz interface window
+InterfaceOptions_AddCategory( MROptionsFrame )
